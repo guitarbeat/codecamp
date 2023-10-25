4120b4e1ecb0d3b58eae0f991104b67b054fb776
Aaron Lorenzo Woods <83775388+guitarbeat@users.noreply.github.com>
2023-10-25 19:25:22 +0000

    Update 3 files

--- src/components/Mediators/index.js
+++ src/components/Mediators/index.js
@@ -1,45 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import { withGoogleSheets } from 'react-db-google-sheets';
-import Loader from 'react-loaders';
-import AnimatedLetters from '../AnimatedLetters';
+// React imports
+import { useEffect, useState } from 'react';
+
+// Third-party libraries and styles
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+// Local imports
 import './index.scss';
 
-
-
-const Mediators = ({ db }) => {
-  const [letterClass, setLetterClass] = useState('text-animate');
+const Mediators = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 1200);
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setLetterClass('text-animate-hover');
-    }, 3000);
-
-    return () => clearTimeout(timeoutId);
+    const handleResize = () => setIsMobile(window.innerWidth < 1200);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return (
-    <>
-      <div className="container mediators-page">
-        <div className="text-zone">
-          <h1>
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={['M', 'e', 'd', 'i', 'a', 't', 'o', 'r', 's']}
-              idx={15}
-            />
-          </h1>
-          <p>
-            Welcome to the Mediators page! Here you can find a list of conflict mediators who are available to assist housing cooperatives.
-          </p>
-          <p>
-            Explore the list below to find a mediator who fits your needs, and feel free to reach out to them directly for assistance.
-          </p>
-                    <h1>Under Construction</h1>
-        </div>
-      </div>
-      <Loader type="pacman" />
-    </>
+    <div className={`container mediators-page ${isMobile ? 'mobile' : 'desktop'}`}>
+      <MediatorText />
+      <MediatorMap />
+    </div>
   );
-}
+};
 
-export default withGoogleSheets('mediators')(Mediators);
+const MediatorText = () => (
+  <div className="text-zone">
+    <h1>Welcome to the Mediators Page!</h1>
+    <p>
+      Here you can find a list of conflict mediators who are available to assist housing cooperatives.
+      Explore the list below to find a mediator who fits your needs, and feel free to reach out to them directly for assistance.
+    </p>
+    {/* Add your list of mediators here */}
+  </div>
+);
+
+const MediatorMap = () => (
+  <div className="map-wrap">
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A sample mediator location.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  </div>
+);
+
+export default Mediators;
